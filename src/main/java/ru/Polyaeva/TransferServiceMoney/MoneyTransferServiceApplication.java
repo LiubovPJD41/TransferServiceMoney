9d4e82b430a4b7d8b5acc1f7569efbc46547278c
Liubov P. JD-41 <lovely8419@mail.ru>
2024-08-15 16:37:02 +0100

    update

--- src/main/java/ru/Polyaeva/TransferServiceMoney/MoneyTransferServiceApplication.java
+++ src/main/java/ru/Polyaeva/TransferServiceMoney/MoneyTransferServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 @SpringBootApplication
 public class MoneyTransferServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MoneyTransferServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTransferServiceApplication.class, args);
+    }
 }
